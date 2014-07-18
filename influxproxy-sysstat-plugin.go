9a2d09ba0cb6f0529dbd344fd75cf5dee7955f0c
Daniel Menet <membership@sontags.ch>
2014-07-18 21:16:41 +0200

    provided scaffold

---
+++ influxproxy-sysstat-plugin.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/influxdb/influxdb-go"
+	"github.com/influxproxy/influxproxy/plugin"
+)
+
+type Functions struct{}
+
+func (f Functions) Describe() plugin.Description {
+	d := plugin.Description{
+		Description: "This plugin takes sysstat reports generated via 'sadf -D -T -- -A' and pushes them to the given influxdb",
+		Author:      "github.com/sontags",
+		Version:     "0.1.0",
+		Arguments: []plugin.Argument{
+			{
+				Name:        "prefix",
+				Description: "Prefix of the series, will be separated with a '.' if given",
+				Optional:    true,
+			},
+		},
+	}
+	return d
+}
+
+func (f Functions) Run(in plugin.Request) plugin.Response {
+	// TODO: implement...
+	var series []*influxdb.Series
+	return plugin.Response{
+		Series: series,
+		Error:  "",
+	}
+}
+
+func main() {
+	f := Functions{}
+	p, err := plugin.NewPlugin()
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		p.Run(f)
+	}
+}
